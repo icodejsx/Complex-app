e0fde95d35bfc9a2c186d1701420894e84c34333
Nweke kenechukwu christopher <72747787+ken286@users.noreply.github.com>
2023-06-16 21:50:28 +0100

    installing ejs engine

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express()
 
+app.set('views', 'views')
+app.set('view engine', 'ejs')
+
 app.get('/', function (raq, res) {
-    res.send('welcome to our new file')
+    res.render('home-guest')
 })
 
 app.listen(5000)
