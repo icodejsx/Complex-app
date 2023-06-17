16bb5d586f57c5871798396605a3c8856f8b28ee
Nweke kenechukwu christopher <72747787+ken286@users.noreply.github.com>
2023-06-17 23:05:45 +0100

    done with routes

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
 const express = require('express');
 const app = express()
-
 const router = require("./router");
-console.log(router)
-
 
 
 app.use(express.static("public"))
 app.set('views', 'views')
 app.set('view engine', 'ejs')
 
-app.get('/', function (raq, res) {
-    res.render('home-guest')
-})
+app.use('/', router)
+
+
 
 app.listen(5000)
--- router.js
+++ router.js
@@ -1,2 +1,10 @@
 const express = require('express');
 const router = express.Router();
+
+router.get('/', function (req, res) {
+    res.render('home-guest');
+})
+
+router.get('/about')
+
+module.exports = router;
