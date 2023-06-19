919680985ca828b5933f2e9ee166299d704e5eed
Nweke kenechukwu christopher <72747787+ken286@users.noreply.github.com>
2023-06-19 20:19:38 +0100

    continue

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express()
 const router = require("./router");
 
+app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static("public"))
 app.set('views', 'views')
