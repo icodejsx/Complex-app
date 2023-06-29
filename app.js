6a293798d85f47147f8c839aed8501d80f366fa5
Nweke kenechukwu christopher <72747787+ken286@users.noreply.github.com>
2023-06-29 15:10:15 +0100

    flashmassage added %100

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')
+const flash = require('connect-flash')
 const app = express()
 
 let sessionOptions = session({
@@ -11,9 +12,10 @@ let sessionOptions = session({
     cookie: { maxAge: 1000 * 60 * 60 * 24, httOnly: true },
 })
 
+app.use(sessionOptions)
+app.use(flash())
 const router = require("./router");
 
-app.use(sessionOptions)
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
