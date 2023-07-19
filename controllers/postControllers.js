f244103623302e46823c6b08163916c6469846d1
Nweke kenechukwu christopher <72747787+ken286@users.noreply.github.com>
2023-07-19 13:30:39 +0100

    working on user delated post

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -29,17 +29,16 @@ exports.viewSingle = async function (req, res) {
 
 exports.viewEditScreen = async function (req, res) {
     try {
-        let post = await Post.findSingleById(req.params.id)
-        if (post.authorId == req.visitorId) {
-            res.render('edit-post', { post: post })
+        let post = await Post.findSingleById(req.params.id, req.visitorId)
+        if (post.isVisitorOwner) {
+            res.render("edit-post", { post: post })
         } else {
-            req.flash("errors", "you did not have permission to perform this action ")
+            req.flash("errors", "You do not have permission to perform that action.")
             req.session.save(() => res.redirect("/"))
         }
     } catch {
         res.render("404")
     }
-
 }
 
 exports.edit = function (req, res) {
