338d0f736e893145ec54eafd861fe8fb4b709a1b
Nweke kenechukwu christopher <72747787+ken286@users.noreply.github.com>
2023-07-21 12:07:31 +0100

    first step workking now

--- frontend-js/modules/search.js
+++ frontend-js/modules/search.js
@@ -1,5 +1,19 @@
 export default class Search {
+    //    1. select dom element and keep track of any usefull data
     constructor() {
-        alert('search JS is available')
+        this.headerSearchIcon = document.querySelector('.header-search-icon')
+        this.events()
+    }
+    // 2.Events 
+    events() {
+        this.headerSearchIcon.addEventListener('click', (e) => {
+            e.preventDefault();
+            this.openOverlay();
+        })
+    }
+
+    // 3. methods
+    openOverlay() {
+        alert('open overlay just ran')
     }
 }
